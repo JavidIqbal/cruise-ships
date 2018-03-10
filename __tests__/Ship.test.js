9008ddd6fdc501e8c676b27a1b0a059c1d4afdf5
JavidIqbal <jav99@hotmail.com>
2018-03-10 14:19:57 +0000

    Stage 2 passed

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,7 +1,18 @@
+/* globals describe it expect */
 const Ship = require('../src/Ship.js');
 
+// const ship = new Ship('Dover');
+
 describe('Ship', () => {
-  it('can be instantiated', () => {
-    expect(new Ship()).toBeInstanceOf(Ship);
+  describe('Ship', () => {
+    it('can be instantiated', () => {
+      expect(new Ship()).toBeInstanceOf(Object);
+    });
+
+  // describe('name', () => {
+  // it('sets the name property', () => {
+  // expect(ship.name).toEqual('Dover');
+  // });
+  // });
   });
 });
